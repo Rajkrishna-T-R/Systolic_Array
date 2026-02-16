3f1abc8bd4f8e16339804fad62dfaea46de5743e
Rajkrishna T R <rajkrishna.thevalil@gmail.com>
2026-02-16 21:22:33 +0530

    Add files via upload

---
+++ MAU.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.02.2026 19:34:38
+// Design Name: 
+// Module Name: MAU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MAU #(parameter data_in_width=32)(
+            input wire [data_in_width-1:0]in_west,
+            input wire [data_in_width-1:0]in_north,
+            input wire clk,
+            input wire rst,
+            output reg [(2*data_in_width)-1:0]result,
+            output reg [data_in_width-1:0]out_east,
+            output reg [data_in_width-1:0]out_south
+            
+    );
+    
+    
+    reg [63:0]acc;
+    always@(posedge clk or negedge rst)
+        begin
+            if(rst==0)
+                begin
+                    result<=64'd0;
+                    out_east<=32'd0;
+                    out_south<=32'd0;
+                    
+                end
+                
+            else
+                begin
+                    out_east<=in_west;
+                    out_south<=in_north;
+                    result<=result+(in_west*in_north);
+                end
+        end
+        
+    
+                    
+    
+    
+    
+endmodule
